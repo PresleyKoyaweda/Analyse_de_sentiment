57ef22de129c956d201c2654de7821a17bba2253
Koyaweda Gervais Presley <gpresleyk@gmail.com>
2025-07-18 16:04:46 -0400

    Correction de preprocessing et requirements

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,22 +1,25 @@
 import re
 import string
 import nltk
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import TweetTokenizer
 
-# 📥 Télécharger automatiquement les ressources NLTK si elles manquent
-for resource in ["stopwords", "wordnet", "omw-1.4"]:
+# 📥 Télécharger automatiquement les ressources nécessaires
+def safe_nltk_download(resource):
     try:
         nltk.data.find(f"corpora/{resource}")
     except LookupError:
         nltk.download(resource)
 
+for res in ["stopwords", "wordnet", "omw-1.4"]:
+    safe_nltk_download(res)
+
 # 🔧 Initialisation
 lemmatizer = WordNetLemmatizer()
 custom_stopwords = set(stopwords.words("english")) | {"rt"}
 
-# 🧼 Fonction de nettoyage de tweet
+# 🧼 Fonction de nettoyage
 def nettoyer_tweet(tweet):
     tweet = re.sub(r'#', '', tweet)
     tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
