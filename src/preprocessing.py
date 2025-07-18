1fa35253d2acf44d1ba1d8473bbfb7f7aa13923a
Koyaweda Gervais Presley <gpresleyk@gmail.com>
2025-07-18 16:01:58 -0400

    Correction de preprocessing_1

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,13 +1,22 @@
 import re
 import string
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import TweetTokenizer
 
+# 📥 Télécharger automatiquement les ressources NLTK si elles manquent
+for resource in ["stopwords", "wordnet", "omw-1.4"]:
+    try:
+        nltk.data.find(f"corpora/{resource}")
+    except LookupError:
+        nltk.download(resource)
+
+# 🔧 Initialisation
 lemmatizer = WordNetLemmatizer()
-stopwords_english = stopwords.words("english")
 custom_stopwords = set(stopwords.words("english")) | {"rt"}
 
+# 🧼 Fonction de nettoyage de tweet
 def nettoyer_tweet(tweet):
     tweet = re.sub(r'#', '', tweet)
     tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
